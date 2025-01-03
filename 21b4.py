95fa53ce3d55f74b2895efc8e52776e09a43349c
Ray Deck <ray@raydeck.com>
2025-01-03 17:27:41 -0500

    feat: cleanup for 21b4 - the solver

--- 21b4.py
+++ 21b4.py
@@ -33,8 +33,6 @@ def test_keypad_path(origin_value, path, destination):
             if value == origin_value:
                 origin_pos = (x, y)
                 break
-    print("Origin position:", origin_pos)
-    print("Large keypad:", large_keypad)
     for move in path:
         if move == "^":
             origin_pos = (origin_pos[0], origin_pos[1] - 1)
@@ -252,157 +250,67 @@ small_pad_controls = ['A', '^', 'v', '<', '>']
 large_pad_controls = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A']
 
 def make_chains_for_level(previous_chains = None, scoreMode = False):
-    old_chain_is_none = False
-    if previous_chains is None:
-        old_chain_is_none = True
-        previous_chains = {}
+    if previous_chains is None: previous_chains = {}
     new_chains = {}
-    used_combos = {}
     for source in small_pad_controls:
         for target in small_pad_controls:
-            if scoreMode == True:
-                new_chains[(source, target)] = 0
-            else:
-                new_chains[(source, target)] = []
-           
+            if scoreMode == True: new_chains[(source, target)] = 0
+            else: new_chains[(source, target)] = []
             # Test all possible sequences of transition ot see which one is the shortest
             shortest_path = None
             combos = multiset_permutations(small_keypad_transitions_no_A[(source, target)])
-                # print("Testing transition from", combo[0], "to", combo[1]
             combolist = list(combos)
-            print("Testing my combos for", source, "to", target, ":", combolist)
-
             for combo in combolist:
-                print("testing combo", combo, "from",small_keypad_transitions_no_A[(source, target)] )  
-                isbadcombo = False
-                if not test_small_keypad_path(source, combo, target):
-                    print("Bad transition from", source, "to", target, "from",combo)
-                    isbadcombo = True 
-                    print("Skipping bad transition", combo, "for",small_keypad_transitions_no_A[(source, target)])
-                    continue
-                else:
-                    print("Found good transition", combo, "for",small_keypad_transitions_no_A[(source, target)])
-                if(scoreMode == True):
-                    temp_path = 0
-                else:
-                    temp_path = []
+                if not test_small_keypad_path(source, combo, target): continue
+                if(scoreMode == True): temp_path = 0
+                else: temp_path = []
                 previous = 'A'
                 for step in combo:
-                    print("Looking at combo step", step)
-                    if scoreMode == True:
-                        temp_path = temp_path + previous_chains.get((previous, step), 1)
-                    else:
-                        temp_path = temp_path + previous_chains.get((previous, step), [step])
+                    if scoreMode == True: temp_path = temp_path + previous_chains.get((previous, step), 1)
+                    else: temp_path = temp_path + previous_chains.get((previous, step), [step])
                     previous = step
-                
                 if scoreMode == True:
-                    if old_chain_is_none == False: temp_path = temp_path + previous_chains.get((previous, "A"), 1)
-                    print("SCORE Temp path is", temp_path)
+                    temp_path = temp_path + previous_chains.get((previous, "A"), 1)
                     if shortest_path == None or temp_path < shortest_path:
-                        print("New shortest path is", temp_path)
                         shortest_path = temp_path
-                        used_combos[(source, target)] = combo
                 else:
-                    if old_chain_is_none == False: temp_path = temp_path + previous_chains.get((previous, "A"), ['A'])
-                    print("LIST Temp path is", ''.join(temp_path))
+                    temp_path = temp_path + previous_chains.get((previous, "A"), ['A'])
                     if shortest_path == None or len(temp_path) < len(shortest_path):
-                        print("New shortest path is", ''.join(temp_path))
                         shortest_path = temp_path
-                        used_combos[(source, target)] = combo
             if shortest_path != None:
                 new_chains[(source, target)] = shortest_path 
             elif scoreMode == True:
                 new_chains[(source, target)] = 0
             else:
                 new_chains[(source, target)] = new_chains.get((source, target), [])
-            if scoreMode == True:
-                print("New chain from", source, "to", target, "is", new_chains[(source, target)])
-            else:
-                print("New chain from", source, "to", target, "is", len(new_chains[(source, target)]))
-    return new_chains, used_combos
+    return new_chains
 
 def make_transition_chains(chain_length, scoreMode = False):
-    previous_chain = None
     saving_chain = None
     chains = []
     for i in range(chain_length):
-        start = datetime.datetime.now()
-        print(f"Current time: {datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S %Z')}")
-        print(f"Looking at keypad {i+1}:")
-        old_chain = saving_chain.copy() if saving_chain != None else None
-        previous_chain, used_combos = make_chains_for_level(saving_chain, scoreMode)
-        saving_chain = previous_chain.copy()
-        for command in saving_chain:
-            if command not in used_combos: print("ERROR Og command is NOT found?", command, command in used_combos)
-            og_command = used_combos[command] # if command in used_combos else small_keypad_transitions_no_A[command]
-            if saving_chain[command] == [] or saving_chain[command] == 0:
-                if scoreMode == True:
-                    saving_chain[command] = saving_chain[command]  + 1
-                else:
-                    saving_chain[command] = saving_chain[command]  + ['A']
-                pass
-            else:
-                if old_chain == None:
-
-                    if scoreMode == True:
-                        saving_chain[command] = saving_chain[command]  + 1
-                    else:
-                        saving_chain[command] = saving_chain[command]  + ['A']
-
-                # else:
-
-                #     if len(og_command) > 0:
-                #         tail_command =  og_command[-1]
-                #         back_to_a = old_chain.get((tail_command, 'A'))
-                #     else:
-                #         print("No tail command found - this should never happen, aborting", command, og_command)
-                #         exit(1)
-                #     saving_chain[command] = saving_chain[command]  + back_to_a
+        saving_chain = make_chains_for_level(saving_chain, scoreMode)
         chains.append(saving_chain)
-
-        end = datetime.datetime.now()
-        print(f"Finished keypad {i+1} in {end-start}")
-        print("After keypad", i+1, "command chains look like:")
-        for command in chains[i]:
-            if(scoreMode == True):
-                print(f"{command}: {chains[i][command]}")
-            else:
-                print(f"{command}: {len(chains[i][command])}")
     return saving_chain
 
 
 def evaluate_sequence(test_sequence, saving_chain, scoreMode = False):
     sequence = test_sequence
-    if scoreMode == False:
-        counter = []
-    else:
-        counter = 0
+    if scoreMode == False: counter = []
+    else: counter = 0
     previous = 'A'
     for i in sequence:
-        print("Trying to move from", previous, "to", i)
         og_pattern = large_keypad_transitions[(previous, i)]
         combos = multiset_permutations(og_pattern)
         combolist = list(combos)
-       
         mincounter = None
         sub_previous = previous
-        # combolist = [og_pattern]
         for pattern in combolist:
             tempcounter = counter
-            print("Trying pattern", pattern)
-            if not test_keypad_path(previous,pattern, i):
-                print("Pattern ", pattern, "from ", previous, "to", i, "is not valid")
-                continue
-
-            print("Pattern from ", previous, "to", i, "is", pattern)
+            if not test_keypad_path(previous,pattern, i): continue
             sub_previous = 'A'
             for j in pattern:
-                print("moving from", sub_previous, " to", j)
                 tempcounter = tempcounter + saving_chain[(sub_previous, j)]
-                if scoreMode == False:
-                    print("after moving to", j, "counter is now", len(tempcounter), ''.join(tempcounter))
-                else:
-                    print("after moving to", j, "counter is now", tempcounter)
                 sub_previous = j
            
             tempcounter = tempcounter + saving_chain[(sub_previous, 'A')]
@@ -412,14 +320,8 @@ def evaluate_sequence(test_sequence, saving_chain, scoreMode = False):
             else:
                 if mincounter == None or len(tempcounter) < len(mincounter):
                     mincounter = tempcounter
-        if(mincounter == None):
-            print("No valid patterns found for", previous, "to", i)
-            break
-        if scoreMode == True:
-            counter = mincounter
-        else:
-            counter = mincounter
-       
+        if(mincounter == None): break
+        counter = mincounter
         previous = i
     print('===============')
     if scoreMode == False:
@@ -435,22 +337,14 @@ def extract_digits(s: str) -> int:
 
 if __name__ == "__main__":
     if(len(sys.argv) < 4):
-        print("Usage: python 21b.py <chain_length> <test_sequence> <scoremode>")    
+        print("Usage: python 21b.py <chain_length> <test_sequence or X for 21.txt contents> <scoremode>")    
         print("Example: python 21b.py 4 1 1")
         sys.exit(1)
     chain_length = int(sys.argv[1])
     test_sequence = sys.argv[2] 
     scoreMode = True if int(sys.argv[3]) > 0 else False
     saving_chain = make_transition_chains(chain_length, scoreMode)
-    print("Final command set as of chain ", chain_length, ":")
-    for command in saving_chain:
-        if(scoreMode == True):
-            print(f"{command}: {saving_chain[command]}")
-        else:
-            print(f"{command}: {len(saving_chain[command])}  ")
-
-    print("===============")
-
+    # print("===============")
     if(test_sequence != "X"):
         print("Evaluating single sequence '", test_sequence, "'")
         l = evaluate_sequence(test_sequence, saving_chain, scoreMode)
@@ -473,4 +367,5 @@ if __name__ == "__main__":
             score = l * digits
             total += score
             print("Score for sequence", seq, "is", l, " * ", digits, " = ", score)
+        print("===============")
         print("Total score is", total)
